a60055342e3e1063c8ef3c4bed26dc9568f22326
Devan Beitel <devan@devanb.us>
2026-09-07 13:29:46 -0400

    correct oxfmt errors

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -3,4 +3,8 @@ import ultracite from "ultracite/oxfmt";
 
 export default defineConfig({
   ...ultracite,
+  ignorePatterns: [
+    ...(ultracite.ignorePatterns ?? []),
+    "app/frontend/components/ui/**",
+  ],
 });
